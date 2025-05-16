6447232a660ec0a5917e3e657b946d0fd98d4d2d
PavinKM <pavinyagava@gmail.com>
2025-05-16 19:12:04 +0530

    Add new files

--- src/login.jsx
+++ src/login.jsx
@@ -1,9 +1,34 @@
+import { useState } from "react"
+
 function Login() {  //starts with captital
+
+    const [pwd1, setPwd1] = useState("")
+    const [pwd2, setPwd2] = useState("")
+
     return (
         <>
-            <p>Login</p>
+            <form className="my-5" style={{width:"50%", margin:"auto"}}>
+                <div className="mb-3">
+                    <label for="exampleInputEmail1" className="form-label">Email address</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div className="mb-3">
+                    <label for="exampleInputPassword1" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1"/>
+                </div>
+                <div className="mb-3">
+                    <label for="exampleInputPassword1" className="form-label">Re-Enter Password</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1"/>
+                </div>
+                <div className="mb-3 form-check">
+                    <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
+                    <label className="form-check-label" for="exampleCheck1">I Agree</label>
+                </div>
+                <button type="submit" className="btn btn-primary">Create Account</button>
+            </form>
         </>
-    )
+    )   //ctrl+F
 }
 
 export default Login

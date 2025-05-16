41a171752eddb0eafd7c7514cd1fd60a3ae45214
PavinKM <pavinyagava@gmail.com>
2025-05-16 21:30:03 +0530

    onchange

--- src/login.jsx
+++ src/login.jsx
@@ -5,6 +5,16 @@ function Login() {  //starts with captital
     const [pwd1, setPwd1] = useState("")
     const [pwd2, setPwd2] = useState("")
 
+    function handlePwd1Change(event){
+        setPwd1(event.target.value)
+        console.log(event)
+    }
+
+    function handlePwd2Change(event){
+        setPwd2(event.target.value)
+        console.log(event)
+    }
+
     return (
         <>
             <form className="my-5" style={{width:"50%", margin:"auto"}}>
@@ -15,11 +25,11 @@ function Login() {  //starts with captital
                 </div>
                 <div className="mb-3">
                     <label for="exampleInputPassword1" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1"/>
+                    <input value={pwd1} onChange={handlePwd1Change} type="password" className="form-control" id="exampleInputPassword1"/>
                 </div>
                 <div className="mb-3">
                     <label for="exampleInputPassword1" className="form-label">Re-Enter Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1"/>
+                    <input value={pwd2} onChange={handlePwd2Change} type="password" className="form-control" id="exampleInputPassword1"/>
                 </div>
                 <div className="mb-3 form-check">
                     <input type="checkbox" className="form-check-input" id="exampleCheck1"/>

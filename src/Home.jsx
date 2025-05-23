e2e11baf6a8c8009cdacefa97cf8c72af6932d66
PavinKM <pavinyagava@gmail.com>
2025-05-23 22:31:24 +0530

    home page refined

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,42 @@
+import { useState,useEffect } from "react";
+import { Link } from "react-router-dom"
+
 function Home(){
+
+    const [posts, setPosts] = useState(null);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/posts')  //http://localhost:3000/posts
+
+        .then(response =>{
+            return response.json()
+        })
+        .then((data) => {
+            console.log(data)
+            setPosts(data)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    },[])
+
     return(
-        <>
-            <h1>Home</h1>
-            <p>This is Home Page</p>
-            <p>Created by Pavin K M . . . </p>
-        </>
+            <div className="container">
+                <Link to="/login">Login</Link>
+            <div className="row justify-content-center m-3">
+                {posts && posts.map(post => {
+                    return (
+                        <div key={post.id} className="card m-3" style={{width: '18rem'}}>
+                            <div className="card-body">
+                                <h5 className="card-title">{post.title}</h5>
+                                <p className="card-text">{post.content}</p>
+                            </div>
+                        </div>
+                    )
+                })
+                }
+            </div>
+            </div>
     );
 }
 
--- src/NotFound.jsx
+++ src/NotFound.jsx
@@ -4,8 +4,10 @@ function NotFound (){
     return(
         <>
             <h2>Page Not Founddd ...</h2>
-            <a className="btn btn-primary" href="/">Home</a>
-        </>
+            {/*<a className="btn btn-primary" href="/">Home</a>    redirect to home page && its send request to server && get response from server and reload*/}
+            <Link className="btn btn-primary" to="/">Home</Link> {/*routes by brouserouter itself */}
+        </>  
+               
     )
 }
 

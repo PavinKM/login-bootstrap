30e1b0e2db65d493b0cceb1d1cb7f03cb29ba3ca
PavinKM <pavinyagava@gmail.com>
2025-05-27 00:30:59 +0530

    posts-post

--- src/Post.jsx
+++ src/Post.jsx
@@ -1,12 +1,34 @@
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 
 function Post(){
 
+    const [post, setPost] = useState(null)
     const {id} = useParams()
+
+        useEffect(()=>{
+                 fetch('http://localhost:3000/posts/'+id)  
+
+            .then(response =>{
+                return response.json()
+            })
+            .then((data) => {
+                console.log(data)
+                setPost(data)
+            })
+            .catch(err => {  //error handling
+                console.log(err)
+            })
+        },[])
 
     return(
         <div>
-            Post : {id}
+            {post && <div>
+                Post : {id}
+                <h2>{post.title}</h2>
+                <h2>{post.content}</h2>
+            </div>
+            }
         </div>
     )
 }
